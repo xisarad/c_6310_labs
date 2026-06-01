95f8f5f552f25e2312db4708ca51dcfe200b9989
xisarad <xisarad@yandex.ru>
2026-06-01 22:14:27 +0300

    Clean up student-10 solutions from all labs

--- lab3-libft/solutions/student-10/src/ft_isdigit.c
+++
@@ -1,7 +0,0 @@
-//Проверка на цифру	Это цифра 0-9?	'5'→1, 'A'→0
-#include "libft.h"
-
-int	ft_isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
--- lab3-libft/solutions/student-10/src/ft_tolower.c
+++
@@ -1,9 +0,0 @@
-//В нижний регистр	БОЛЬШУЮ букву → маленькую	'A'→'a', 'a'→'a'
-#include "libft.h"
-
-int	ft_tolower(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (c + 32);
-	return (c);
-}
