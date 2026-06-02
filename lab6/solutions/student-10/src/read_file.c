1737d8a3f1d84911160b79be83ba990c957d3c01
xisarad <xisarad@yandex.ru>
2026-06-02 22:11:02 +0300

    Lab6: fix read_file - remove conflicting static functions, use libft

--- lab6/solutions/student-10/src/read_file.c
+++ lab6/solutions/student-10/src/read_file.c
@@ -2,14 +2,48 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static char	*get_next_line(int fd)
+static int	count_lines_and_max_len(const char *filename, int *max_len)
+{
+	int		fd;
+	char	buf[1];
+	int		bytes;
+	int		lines;
+	int		current_len;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	lines = 1;
+	current_len = 0;
+	*max_len = 0;
+	bytes = read(fd, buf, 1);
+	while (bytes > 0)
+	{
+		if (buf[0] == '\n')
+		{
+			lines++;
+			if (current_len > *max_len)
+				*max_len = current_len;
+			current_len = 0;
+		}
+		else
+			current_len++;
+		bytes = read(fd, buf, 1);
+	}
+	close(fd);
+	if (current_len > *max_len)
+		*max_len = current_len;
+	return (lines);
+}
+
+static char	*read_line(int fd, int max_len)
 {
 	char	*buffer;
 	char	c;
 	int		i;
 	int		bytes;
 
-	buffer = (char *)malloc(1024);
+	buffer = (char *)malloc(max_len + 2);
 	if (!buffer)
 		return (NULL);
 	i = 0;
@@ -19,11 +53,6 @@ static char	*get_next_line(int fd)
 		buffer[i++] = c;
 		bytes = read(fd, &c, 1);
 	}
-	if (bytes <= 0 && i == 0)
-	{
-		free(buffer);
-		return (NULL);
-	}
 	buffer[i] = '\0';
 	return (buffer);
 }
@@ -32,30 +61,37 @@ char	**read_file(const char *filename)
 {
 	int		fd;
 	char	**lines;
-	char	*line;
-	int		count;
+	int		total_lines;
+	int		max_len;
+	int		i;
 
+	total_lines = count_lines_and_max_len(filename, &max_len);
+	if (total_lines <= 0)
+		return (NULL);
+	lines = (char **)malloc((total_lines + 1) * sizeof(char *));
+	if (!lines)
+		return (NULL);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		return (NULL);
-	lines = NULL;
-	count = 0;
-	line = get_next_line(fd);
-	while (line)
 	{
-		lines = (char **)realloc(lines, (count + 2) * sizeof(char *));
-		if (!lines)
+		free(lines);
+		return (NULL);
+	}
+	i = 0;
+	while (i < total_lines)
+	{
+		lines[i] = read_line(fd, max_len);
+		if (!lines[i])
 		{
-			free(line);
+			while (--i >= 0)
+				free(lines[i]);
+			free(lines);
 			close(fd);
 			return (NULL);
 		}
-		lines[count] = line;
-		count++;
-		line = get_next_line(fd);
+		i++;
 	}
+	lines[total_lines] = NULL;
 	close(fd);
-	if (lines)
-		lines[count] = NULL;
 	return (lines);
 }
